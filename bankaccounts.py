76827153be1ebdbddf08509077b4f201c7977647
ShaGrib <81344044+ShaGrib@users.noreply.github.com>
2021-12-01 15:03:41 -0500

    first commit

---
+++ bankaccounts.py
@@ -0,0 +1,66 @@
+class User:
+    bank_name = "First National Dojo"
+    def __init__(self, name, email, account):
+        self.name = name
+        self.email = email
+        self.account = BankAccount()
+    def make_deposit(self, amount):
+        print(f"{self.name} hands {amount} to the teller")
+        self.account.deposit(amount)
+        return self
+    def make_withdrawal(self, amount):
+        print(f"{self.name} asks the teller for {amount} from the account")
+        self.account.withdraw(amount)
+        return self
+    def display_user_balance(self):
+        print(f"{self.name} asks the teller to show the balance of the account")
+        self.account.yield_interest()
+        self.account.display_account_info()
+
+# deposit(self, amount) - increases the account balance by the given amount
+# withdraw(self, amount) - decreases the account balance by the given amount if there are sufficient funds;
+# if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
+# display_account_info(self) - print to the console: eg. "Balance: $100"
+# yield_interest(self) - increases the account balance by the current balance * the interest rate
+# (as long as the balance is positive)
+
+class BankAccount:
+    def __init__(self, int_rate = 1.02, balance= 0): 
+        self.interest = int_rate
+        self.balance = balance
+        # don't worry about user info here; we'll involve the User class soon
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        if self.balance <= 0:
+            print(f"Insufficient funds: Charging a $5 Fee")
+            self.balance -= 5
+        else:
+            self.balance -= amount
+        return self
+    def display_account_info(self):
+        print(f"Balance: ${(float(self.balance))}")
+    def yield_interest(self):
+        if self.balance >= 1:
+            float(self.balance)
+            self.balance *= self.interest
+            int(self.balance)
+        return self
+    # def display_all_accounts(User):
+    #     print(f"Accounts {User.account}")
+
+guido = User("Guido der Rothchild", "guido@python.com", "4413223")
+monty = User("Monty Python", "monty@python.com", "4413224")
+armen = User("Armen van Tollen", "armen@moomoomail.com", "4413225")
+guido.bank_name = "Dojo Credit Union"
+# bank = BankAccount()
+
+guido.display_user_balance()
+monty.display_user_balance()
+armen.display_user_balance()
+
+guido.make_deposit(950).make_deposit(674).make_deposit(783).make_withdrawal(305).display_user_balance()
+monty.make_deposit(1000).make_deposit(2500).display_user_balance()
+armen.make_deposit(100).make_withdrawal(87).make_withdrawal(64).make_withdrawal(10).display_user_balance()
+
